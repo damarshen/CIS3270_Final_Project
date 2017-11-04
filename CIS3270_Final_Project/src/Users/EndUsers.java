8b9427015b8859a106d9e14faf42d1f7a6cf1349
ojall <ousainouj@gmail.com>
2017-11-04 19:42:04 -0400

    First commit

---
+++ CIS3270_Final_Project/src/Users/EndUsers.java
@@ -0,0 +1,18 @@
+package Users;
+
+public class EndUsers {
+private String firstName;
+private String lastName;
+private String Address;
+private int zipCode;
+private String state;
+private String username;
+private String password;
+private String email;
+private String ssn;
+private String securityQuestion;
+private String securityAnswer;
+
+
+
+}
