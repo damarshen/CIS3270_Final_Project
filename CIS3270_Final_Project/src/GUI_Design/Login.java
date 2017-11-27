54a928aa68f0aac113d1a5f6b03146aaae1b80f2
ojall <ousainouj@gmail.com>
2017-11-26 21:00:52 -0500

    update

--- CIS3270_Final_Project/src/GUI_Design/Login.java
+++ CIS3270_Final_Project/src/GUI_Design/Login.java
@@ -13,6 +13,7 @@ public class Login {
 	//query variable
 	ResultSet myRs;
 	
+	public static String user ="";
 	//Jframe variable
 	private static JFrame frame;
 	//Creates a text field
@@ -53,7 +54,7 @@ public class Login {
 	private void initialize() {
 		//GUI frame
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(450, 200, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setVisible(false);
@@ -84,8 +85,64 @@ public class Login {
 		frame.getContentPane().add(textFieldUSer);
 		textFieldUSer.setColumns(10);
 		
-		//Creates password field for password to be entered
+		//Creates password field for password to be entered with action listener for enter key
 		passwordField = new JPasswordField();
+		passwordField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				//Exception handling for connecting to the database
+				try {
+				//get a connection to the database
+				Connection myConn = DriverManager.getConnection("jdbc:mysql://35.193.248.221:3306/?verifyServerCertificate=false&useSSL=true", "root", "Tdgiheay12");
+				//create a statement
+				Statement myStat = myConn.createStatement();
+				//execute a query
+				ResultSet myRs;	
+					
+				//collects user name from the user name text field and assigns to a string called user
+				user = textFieldUSer.getText().trim();
+				
+				//Collects password from the password text field and assigns to a string called password
+				String password = passwordField.getText().trim();
+				
+				//SQL query to check if user  name and password is in database
+				String sqlUserCheck = "SELECT `username` FROM `flights`.`user` where username = '" + user + "' and password = '" + password + "'";
+				myRs = myStat.executeQuery(sqlUserCheck);
+				
+				//Creates a variable for future checking
+				int count = 0;
+				
+				//While loop that will determine if user is in the database
+				while(myRs.next()){
+					
+					count = count + 1;
+					
+					}
+				
+				//If user is in the database and the password is correct it it will take user to main page
+				if (count==1) {
+					Login.frame.setVisible(false);
+					mainPage info =new mainPage();
+					mainPage.main(null);
+					
+				}
+				
+				/**If user is not in database or password is incorrect, an error message is displayed prompting 
+				 *  change in user name or password, attempt password recovery, 
+				 *  or prompts the user to register if they do not have an account
+				**/
+				else if (count<1) {
+					JOptionPane.showMessageDialog(null, "Username and password combination is either incorrect or the account does not exist.\n Please select The forgot password if your password is unknonwn, or the register option to create an account.");
+				}
+				
+				}
+			
+				catch(Exception ex){
+					
+				}
+			}
+		});
 		passwordField.setBounds(161, 121, 124, 20);
 		frame.getContentPane().add(passwordField);
 		
@@ -104,7 +161,7 @@ public class Login {
 				ResultSet myRs;	
 					
 				//collects user name from the user name text field and assigns to a string called user
-				String user = textFieldUSer.getText().trim();
+				user = textFieldUSer.getText().trim();
 				
 				//Collects password from the password text field and assigns to a string called password
 				String password = passwordField.getText().trim();
@@ -168,7 +225,7 @@ public class Login {
 		JButton btnNewButton = new JButton("Exit");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				JOptionPane.showMessageDialog(null, "The program will now exit");
+				JOptionPane.showMessageDialog(null, "The program will now exit.");
 				System.exit(0);
 			}
 		});
